39d91c06d9d9956d8ab14fef5d437851c6aa8e35
LongPQ2 <longpq33@gmail.com>
2026-01-28 17:22:53 +0700

    fix build

--- src/components/ErrorDisplayContent.tsx
+++ src/components/ErrorDisplayContent.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Alert, Tag } from 'antd';
+import { Alert } from 'antd';
 import type { SimulateResponse } from '../api/simulate';
 
 interface ErrorDisplayContentProps {
